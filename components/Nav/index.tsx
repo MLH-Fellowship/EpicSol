78f74c60cac8c5260a154755cf89967a6577e092
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2022-02-04 12:21:06 +0000

    update nav ui

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { signIn, useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import Link from 'next/link';
 import { Popover } from "@mui/material";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Navbar = () => {
+  const router = useRouter();
   const { data: session } = useSession();
   const [anchorEl, setAnchorEl] = useState(null);
+  const cart = 0;
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -19,9 +24,25 @@ const Navbar = () => {
       <nav
         className="bg-appGray h-[52px] flex flex-row items-center px-[20px] justify-between"
       >
-        <p className="text-appGray1 uppercase text-[14px] tracking-widest font-medium">Game Store</p>
+        <Link href={'/'} passHref={true}>
+          <p
+            className="text-appGray1 uppercase text-[14px] tracking-widest font-medium cursor-pointer"
+          >Game Store</p>
+        </Link>
         {session ? (
-          <>
+          <div className="flex flex-row items-center space-x-8">
+            <button
+              disabled={cart === 0}
+              onClick={() => router.push('/cart')}
+              className="flex flex-row items-center tracking-wider text-appGray1"
+            >
+              <FiShoppingCart size={20}/>
+              {cart !== 0 && (
+                <span className="px-[10px] ml-2 bg-white rounded-xl text-black font-bold">
+                  {cart}
+                </span>
+              )}
+            </button>
             <button
               aria-describedby={"simple-popover"}
               onClick={handleClick}
@@ -46,7 +67,7 @@ const Navbar = () => {
                 Sign Out
               </button>
             </Popover>
-          </>
+          </div>
         ) : (
           <button
             className="font-medium tracking-widest text-appGray1"
